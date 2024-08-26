13b57b47bc58a1b7f45c227fb568dd02ed0ed1cf
OneClickTechy <oneclicktechy@gmail.com>
2024-08-26 19:50:33 +0530

    finished

--- app.js
+++ app.js
@@ -1,199 +1,237 @@
-// let buttons = document.querySelectorAll(".for-calc");
-// const _delete = document.querySelector('#delete');
-// const deleteIcon = document.getElementById('#delete-icon');
-// let displayValue = "";
-// let display = document.getElementById("output");
-// let dataStore = [];
-// let buffer = "";
-// const operators = /[\*\/\+%-]/;
-// const percent = /[%]/;
-// const numbers = /[\d.]/;
-// const plusOrMinus = /[\+-]/;
-// let openParenthesesCount = 0;
-// const calc = /[=]/;
-// let answer = null;
+const display = document.querySelector('.calc-display');//display
+const keys = document.querySelector('.calc-buttons');//calculator keys 
 
-// buttons.forEach((element) => {
-//   element.addEventListener("click", (e) => {
-    
-//     //checke if input is equal to an % operator
-//     if(e.target.value === '()'){
-//       console.log('inside bracket')
-//       if(buffer === '' && dataStore.length === 0){
-//         console.log('first')
-//         dataStore.push('(');
-//         openParenthesesCount++;
-//         console.log(dataStore, openParenthesesCount);
-//       }else if (dataStore[dataStore.length-1] === '('){
-//         if(buffer !== ''){
-//           dataStore.push(buffer);
-//           buffer = '';
-//           dataStore.push(')');
-//           openParenthesesCount--;
-//         }
-//       }else if(numbers.test(buffer)){
-//         dataStore.push(buffer);
-//         buffer='';
-//         dataStore.push('*');
-//         dataStore.push('(');
-//       }
-//     }
-//     else if (percent.test(e.target.value)) {
-//       if (buffer != "") {
-//         dataStore.push(buffer);
-//         dataStore.push("/");
-//         dataStore.push("100");
-//         dataStore.push("*");
-//         buffer = "";
-//       }
-//       //check if input is equal to an operator +-*/
-//     } else if (operators.test(e.target.value)) {
-//       console.log(e.target.value);
-//       if (buffer != "") {
-//         dataStore.push(buffer);
-//         buffer = "";
-//         dataStore.push(e.target.value);
-//       } else if (buffer == "") {
-//       }
-//       //check if input is equal to a number
-//     }else if (numbers.test(e.target.value)) {
-//       console.log(e.target.value);
-//       buffer += e.target.value;
+let expression = [];
+let buffer = '';
+let parenthesesBug = 0;
 
-//       //calc
-//     } else if (calc.test(e.target.value)) {
-//       console.log(e.target.value);
-//       if (dataStore.length >= 2 && buffer !== "") {
-//         dataStore.push(buffer);
-//         buffer = "";
 
-//         while (dataStore.indexOf("%") >= 0) {
-//           const index = dataStore.indexOf("%");
-//           const result =
-//             parseFloat(dataStore[index - 1]) / parseFloat(dataStore[index + 1]);
-//           dataStore.splice(index - 1, 3, result);
-//           console.log(dataStore);
-//           console.log(result);
-//         }
+//add event listener to calc keys
+keys.addEventListener('click', (e) => {
+    const { target } = e;
+    const { value } = target;
 
-//         while (dataStore.indexOf("/") >= 0) {
-//           const index = dataStore.indexOf("/");
-//           const result =
-//             parseFloat(dataStore[index - 1]) / parseFloat(dataStore[index + 1]);
-//           dataStore.splice(index - 1, 3, result);
-//           console.log(dataStore);
-//           console.log(result);
-//         }
+    if (/\d/.test(value)) {//if enter number
+        buffer += value;
+        console.log(buffer);
 
-//         while (dataStore.indexOf("*") >= 0) {
-//           const index = dataStore.indexOf("*");
-//           const result =
-//             parseFloat(dataStore[index - 1]) * parseFloat(dataStore[index + 1]);
-//           dataStore.splice(index - 1, 3, result);
-//           console.log(dataStore);
-//           console.log(result);
-//         }
+    } else if (value === '.') {//if enter dot
+        if (!buffer.includes(value) && /\d/.test(buffer[buffer.length - 1])) {
+            console.log(true);
+            buffer += value;
+            console.log(buffer);
 
-//         while (dataStore.indexOf("+") >= 0) {
-//           const index = dataStore.indexOf("+");
-//           const result =
-//             parseFloat(dataStore[index - 1]) + parseFloat(dataStore[index + 1]);
-//           dataStore.splice(index - 1, 3, result);
-//           console.log(dataStore);
-//           console.log(result);
-//         }
+        } else if (buffer === '') {
+            buffer += `0${value}`;
+            console.log(buffer);
+        } else { console.log(false) }
+        console.log(value);
+    } else if (target.classList.contains('operator')) {//if enter operator
+        console.log('operator' + value);
+        if (buffer !== '' || /^-?\d+(\.\d)?$/.test(expression[expression.length - 1])) {
+            console.log('buffer pushed');
+            expression.push(buffer);
+            console.log(expression);
+            buffer = '';
+            console.log(`${buffer} is empty`);
+            expression.push(value);
+            console.log(expression)
+        } else if (buffer === '' && expression[expression.length - 1] === ')') {
+            expression.push(value);
+        }
 
-//         while (dataStore.indexOf("-") >= 0) {
-//           const index = dataStore.indexOf("-");
-//           const result =
-//             parseFloat(dataStore[index - 1]) - parseFloat(dataStore[index + 1]);
-//           dataStore.splice(index - 1, 3, result);
-//           console.log(dataStore);
-//           console.log(result);
-//         }
-//       }
-//     } else if (e.target.value == "clear") {
-//       buffer = "";
-//       let answer = null;
-//       dataStore = [];
-//       display.value = "";
-//       result = null;
-//     }
-//     console.log(buffer);
-//     console.log(dataStore);
+    } else if (target.classList.contains('operator-percentage')) {//if enter %
+        console.log(value);
+        if (buffer !== '') {
+            expression.push(String(parseFloat(buffer) / 100));
+            buffer = '';
+            console.log(expression);
+        }
+    } else if (value === '()') {//if enter target is ()
+        console.log('()');
+        if (buffer !== '') {
+            expression.push(buffer);
+            buffer = '';
+            console.log('buffer is not empty so pushed', expression);
+        }
+        if (parenthesesBug === 0 & expression.length === 0) {//from begin () is clicked
+            expression.push('(');
+            console.log(expression);
+            parenthesesBug++;
+        } else if (expression[expression.length - 1] === '/' ||
+            expression[expression.length - 1] === '*' ||
+            expression[expression.length - 1] === '+' ||
+            expression[expression.length - 1] === '-'
+        ) {
+            expression.push('(');
+            parenthesesBug++;
+        } else if (/^-?\d+(\.\d)?$/.test(expression[expression.length - 1])) {//if click after number
+            if (parenthesesBug > 0) {
+                expression.push(')');
+                parenthesesBug--;
+                console.log(expression);
+            } else if (parenthesesBug === 0) {
+                expression.push('*', '(');
+                parenthesesBug++;
+                console.log(expression);
+            }
+        } else if (expression[expression.length - 1] === '(') {
+            expression.push('(');
+            parenthesesBug++;
+        } else if (expression[expression.length - 1] === ')') {
+            if (parenthesesBug > 0) {
+                expression.push(')');
+                parenthesesBug--;
+            } else if (parenthesesBug === 0) {
+                expression.push('*', '(');
+                parenthesesBug++;
+            }
 
-//    // Update the display to show current buffer and stored operations
-// display.value = dataStore.join(" ") + " " + buffer;
-//   });
-// });
+        }
 
-//test
+    } else if (value === '+|-') {//if value is plus or minus
+        if (buffer.includes('-')) {
+            console.log(`${buffer} have -`);
+            buffer = buffer.slice(1);
+            console.log(buffer);
+        } else {
+            console.log('no -');
+            buffer = '-' + buffer;
+            console.log(buffer);
+        }
+        console.log('sign ', value);
+    } else if (value === '=') {//if calculate is trigger
+        console.log('calculate begin');
+        if (buffer !== '') {
+            expression.push(buffer);
+            buffer = '';
+            console.log(expression);
+        }
+        if(parenthesesBugCheck(expression)){
+            console.log('parenthesis are equal\n', expression);
+            display.innerText = '';
+            display.innerText = calculator(splicer(expression));
+        }else{
+            display.innerText = 'error';
+        }
+    } else if (value === 'C') {//if value is clear
+        console.log('clear');
+        buffer = '';
+        expression = [];
+        parenthesesBug = 0;
+        display.innerText = null;
+    }
+    console.log('buffer :', buffer);
+    console.log('exp : ', expression);
+    console.log('parC', parenthesesBug);
 
-arr = ['10', '*', '(', '5', '+', '4', '-', '3', '/', '2', ')', '/', '3'];
+display.innerText = expression.join('') + ' ' + buffer;
+})
+const array = ['(', ')', '('];
 
-while(arr.lastIndexOf('(') >= 0){
-let initIndex = arr.lastIndexOf('(');
-let finIndex = null;
-for(let i = initIndex+1; i < arr.length; i++){
-  if(arr[i]===')'){
-    finIndex = i;
-    break;
-  }
+//parenthesis balance checker function
+function parenthesesBugCheck(array) {
+    let opc = 0;
+    let cpc = 0;
+
+    array.map((i) => {
+        if(i === '('){
+            opc++;
+        }else if(i === ')'){
+            cpc++;
+        }
+    })
+    return (opc === cpc) ? true : false;
+
 }
-let betweenCount = finIndex - initIndex -1;
-console.log(initIndex, finIndex, betweenCount);
-let nestedArray = [];
 
-for(let i=initIndex+1; i < finIndex; i++){
-  nestedArray.push(arr[i]);
-}
-console.log(nestedArray);
-while (nestedArray.indexOf("%") >= 0) {
-            const index = nestedArray.indexOf("%");
-            const result =
-              parseFloat(nestedArray[index - 1]) / parseFloat(nestedArray[index + 1]);
-            nestedArray.splice(index - 1, 3, result);
-            console.log(nestedArray);
-            console.log(result);
-          }
-  
-          while (nestedArray.indexOf("/") >= 0) {
-            const index = nestedArray.indexOf("/");
-            const result =
-              parseFloat(nestedArray[index - 1]) / parseFloat(nestedArray[index + 1]);
-            nestedArray.splice(index - 1, 3, result);
-            console.log(nestedArray);
-            console.log(result);
-          }
-  
-          while (nestedArray.indexOf("*") >= 0) {
-            const index = nestedArray.indexOf("*");
-            const result =
-              parseFloat(nestedArray[index - 1]) * parseFloat(nestedArray[index + 1]);
-            nestedArray.splice(index - 1, 3, result);
-            console.log(nestedArray);
-            console.log(result);
-          }
-  
-          while (nestedArray.indexOf("+") >= 0) {
-            const index = nestedArray.indexOf("+");
-            const result =
-              parseFloat(nestedArray[index - 1]) + parseFloat(nestedArray[index + 1]);
-            nestedArray.splice(index - 1, 3, result);
-            console.log(nestedArray);
-            console.log(result);
-          }
-  
-          while (nestedArray.indexOf("-") >= 0) {
-            const index = nestedArray.indexOf("-");
-            const result =
-              parseFloat(nestedArray[index - 1]) - parseFloat(nestedArray[index + 1]);
-            nestedArray.splice(index - 1, 3, result);
-            console.log(nestedArray);
-            console.log(result);
-          }
+//calculator function
 
-          
-          console.log(nestedArray);
-break;
+
+//splicer function uses: split the array from ( to ) and calculate inner values and return single value
+function splicer(array){
+    while(array.includes('(')){
+    if(!casetest1.includes('(')) {
+     console.log('no ()');   
+    }
+    let obi = array.lastIndexOf('(');
+    let cbi;
+    for(let i = obi + 1; obi < array.length - 1; i++){
+        if(array[i] === ')'){
+            cbi = i;
+            break;
+        }
+    }
+    let tempArr = array.slice(obi+1, cbi);
+    let result = calculator(tempArr);
+    console.log(result);
+    console.log(obi, cbi);
+    const count = cbi - obi + 1;
+    array.splice(obi, count, String(result));
 }
+    return array;
+}
+console.log(calculator(splicer(casetest1)));
+
+function calculator(array) {
+    let ans;
+
+    // division and multiplication 
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === '/') {
+            ans = parseFloat(array[i - 1]) / parseFloat(array[i + 1]);
+            array.splice(i - 1, 3, ans);
+            i--; // Adjust index after splicing
+        } else if (array[i] === '*') {
+            ans = parseFloat(array[i - 1]) * parseFloat(array[i + 1]);
+            array.splice(i - 1, 3, ans);
+            i--; // Adjust index after splicing
+        }
+    }
+
+    // addition and subtraction
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === '+') {
+            ans = parseFloat(array[i - 1]) + parseFloat(array[i + 1]);
+            array.splice(i - 1, 3, ans);
+            i--; // Adjust index after splicing
+        } else if (array[i] === '-') {
+            ans = parseFloat(array[i - 1]) - parseFloat(array[i + 1]);
+            array.splice(i - 1, 3, ans);
+            i--; // Adjust index after splicing
+        }
+    }
+
+    // The final result should be the only element left in the array
+    return array[0];
+}
+
+//test cases from chatgpt to check my logic is correct or not
+// Test cases array
+const testCases = [
+    { expression: ['2', '+', '3'], expected: 5 },               // Simple addition
+    { expression: ['10', '-', '5'], expected: 5 },              // Simple subtraction
+    { expression: ['4', '*', '5'], expected: 20 },              // Simple multiplication
+    { expression: ['20', '/', '4'], expected: 5 },              // Simple division
+    { expression: ['2', '+', '3', '*', '4'], expected: 14 },    // BODMAS rule
+    { expression: ['(', '2', '+', '3', ')', '*', '4'], expected: 20 }, // Parentheses with multiplication
+    { expression: ['10', '+', '2', '*', '6', '/', '3'], expected: 14 }, // Complex expression
+    { expression: ['10', '/', '2', '*', '(', '3', '+', '2', ')'], expected: 25 }, // Mixed operations
+    { expression: ['(', '10', '+', '2', ')', '*', '(', '3', '+', '4', ')'], expected: 84 }, // Nested parentheses
+    { expression: ['100', '-', '(', '50', '/', '2', ')'], expected: 75 } // Subtraction with division
+];
+
+// Function to test the calculator
+function testCalculator() {
+    testCases.forEach((test, index) => {
+        const result = calculator(splicer(test.expression));
+        const passed = result === test.expected;
+
+        console.log(`Test Case ${index + 1}: ${test.expression.join(' ')} = ${result}`);
+        console.log(passed ? 'Passed' : `Failed (Expected: ${test.expected})`);
+        console.log('-----------------------------------------');
+    });
+}
+
+// Run the test
+testCalculator();
