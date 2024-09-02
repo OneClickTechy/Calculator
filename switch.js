af59acbf26360db2c1109cc6d9a2523f1a6748f4
OneClickTechy <oneclicktechy@gmail.com>
2024-09-02 18:14:51 +0530

    restructured the html file

--- switch.js
+++ switch.js
@@ -1,14 +1,11 @@
-const switchContainer = document.querySelector('.switch-container');
 const _switch = document.querySelector('.switch');
-const body = document.body;
 
-switchContainer.addEventListener("click", () => {
-  _switch.classList.toggle('active');
-
-  // Toggle dark mode
-  if (_switch.classList.contains('active')) {
-    body.setAttribute('data-theme', 'dark');
-  } else {
-    body.setAttribute('data-theme', 'light');
+_switch.addEventListener('click', ()=>{
+  if(_switch.classList.contains('fa-sun')){
+    _switch.classList.remove('fa-sun');
+    _switch.classList.add('fa-moon');
+  }else{
+    _switch.classList.add('fa-sun');
+    _switch.classList.remove('fa-moon');
   }
-});
+})
